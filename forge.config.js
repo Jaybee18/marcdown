51ee008e2162d8d28af9fdf0d62a364b22b345e8
Jan Bessler <mister.jaybee18@gmail.com>
2022-12-12 11:08:22 +0100

    initial commit

---
+++ forge.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        certificateFile: './cert.pfx',
+        certificatePassword: process.env.CERTIFICATE_PASSWORD,
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
---
+++ renderer.js
@@ -0,0 +1,39 @@
+const parse = require("marked").parse;
+const electron = require("electron");
+
+let editor = document.querySelector(".editor");
+let textfield = document.querySelector(".textfield");
+let renderview = document.querySelector(".renderview");
+let resizehandle = document.querySelector(".resizehandle");
+let draghandle = document.querySelector(".toolbar");
+
+function render() {
+    parse(textfield.value, (err, res) => {
+        renderview.innerHTML = res;
+    });
+}
+
+// editor
+textfield.addEventListener("input", (e) => {
+    render();
+});
+
+// view resizing (editor / renderer)
+function resize(e) {
+    editor.style.width = e.x / window.innerWidth * 100 + "%";
+    renderview.style.width = (1 - e.x / window.innerWidth)*100 + "%";
+    draghandle.style.width = (1 - e.x / window.innerWidth)*100 + "%";
+  }
+  resizehandle.addEventListener("mousedown", () => {
+    document.addEventListener("mousemove", resize);
+  });
+  document.addEventListener("mouseup", () => {
+    document.removeEventListener("mousemove", resize);
+  });
+
+// close btn
+let closebtn = document.querySelector(".closebtn");
+closebtn.addEventListener("click", () => {
+    console.log("close")
+    electron.ipcRenderer.invoke("close");
+});
